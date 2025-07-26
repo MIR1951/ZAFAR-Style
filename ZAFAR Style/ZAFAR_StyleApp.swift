4fe9d0ac066229ed18ab5c4cbc012c80a2913f1e
MIR1951 <160203220+MIR1951@users.noreply.github.com>
2025-07-26 20:28:13 +0500

    Initial Commit

---
+++ ZAFAR Style/ZAFAR_StyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZAFAR_StyleApp.swift
+//  ZAFAR Style
+//
+//  Created by Kenjaboy Xajiyev on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ZAFAR_StyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZAFAR StyleTests/ZAFAR_StyleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZAFAR_StyleTests.swift
+//  ZAFAR StyleTests
+//
+//  Created by Kenjaboy Xajiyev on 26/07/25.
+//
+
+import Testing
+@testable import ZAFAR_Style
+
+struct ZAFAR_StyleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
